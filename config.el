05acdbeac42ff8b13cb50be4add4e79bb2243bfe
Pedro Rodrigues <pedrohenriquerls@gmail.com>
2020-07-31 15:35:28 -0300

    Doomfig

---
+++ config.el
@@ -0,0 +1,90 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Pedro Rodrigues"
+      user-mail-address "pedrohenriquerls@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+(setenv "FZF_DEFAULT_COMMAND" "rg --files --hidden --follow --no-ignore-vcs -g \"!{node_modules,.git,*.gem,*.log,*.class,vendor,logs,tmp,build,target,*.jar,public,build,.idea,.gradle,.DS_Store}\"")
+(setq flycheck-ruby-rubocop-executable "~/.rbenv/shims/rubocop")
+(after! ivy
+  :config
+  (dolist (key '("\M-p" "\C-p"))
+    (global-unset-key key)
+    (undefine-key! key))
+  (global-set-key [remap projectile-find-file] 'fzf-projectile)
+  (global-set-key (kbd "M-p") 'fzf-projectile)
+  (define-key! (kbd "M-p") 'fzf-projectile))
+;;(add-to-list 'projectile-globally-ignored-directories "*node_modules")
+;;(add-to-list 'projectile-globally-ignored-directories "*vendor")
+;;(add-to-list 'projectile-globally-ignored-directories "*tmp")
+;;(add-to-list 'projectile-globally-ignored-directories "*log")
+;;(add-to-list 'projectile-globally-ignored-files "*.log")
+;;(add-to-list 'projectile-globally-ignored-files "*.gem")
+;;(add-to-list 'projectile-globally-ignored-files "*.class")
+;;(add-to-list 'projectile-globally-ignored-files "*.meta")
+;;(add-to-list 'projectile-globally-ignored-files "*.asset")
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((projectile-globally-ignored-directories . "*node_modules*")
+     (projectile-globally-ignored-directories . "./vendor/")
+     (projectile-globally-ignored-directories . "./vendor/cache/")
+     (projectile-globally-ignored-directories . "vendor")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
